2ca253fb6c4e03a470ab626b1e6c90ff8ed20b5a
Oleksandra Myshkina <myshkinasasha@gmail.com>
2020-04-21 20:22:24 +0300

    Initial Commit

---
+++ СaTinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  СaTinder
+//
+//  Created by Sasha Myshkina on 21.04.2020.
+//  Copyright © 2020 Sasha Myshkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
